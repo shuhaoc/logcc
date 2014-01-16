bc92c5243abf410106a901dc4bd61f77b7071d5f
shuhaoc <shuhaoc@qq.com>
2014-01-17 00:28:13 +0800

    更新updatemrl.bat

--- updatemrl.bat
+++ updatemrl.bat
@@ -1,6 +1,9 @@
 set MRL_ROOT=..\mrl
 set PROJECT_ROOT=.
 
+msbuild /p:Configuration=Debug %MRL_ROOT%\mrl.sln
+msbuild /p:Configuration=Release %MRL_ROOT%\mrl.sln
+
 xcopy /e /y /i /f %MRL_ROOT%\include\mrl %PROJECT_ROOT%\include\mrl
 xcopy /e /y /i /f %MRL_ROOT%\lib\Debug\*.lib %PROJECT_ROOT%\lib\Debug\
 xcopy /e /y /i /f %MRL_ROOT%\bin\Debug\*.dll %PROJECT_ROOT%\bin\Debug\
