62bb75c31deae3f062001334d4b965faecc1a6c1
shuhaoc <shuhaoc@qq.com>
2014-01-18 23:00:10 +0800

    删除CWinUIDoc中无用方法

--- src/WinUI/WinUIDoc.cpp
+++ src/WinUI/WinUIDoc.cpp
@@ -52,7 +52,6 @@ void CWinUIDoc::setQueryResult(LogQueryResult* queryResult) {
 	this->queryResult = queryResult;
 }
 
-// UNDONE: 重写此方法，禁用新建
 //BOOL CWinUIDoc::OnNewDocument()
 //{
 //	if (!CDocument::OnNewDocument())
@@ -67,85 +66,85 @@ void CWinUIDoc::setQueryResult(LogQueryResult* queryResult) {
 // CWinUIDoc 序列化
 
 // UNDONE: 删除
-void CWinUIDoc::Serialize(CArchive& ar)
-{
-	if (ar.IsStoring())
-	{
-		// TODO: 在此添加存储代码
-	}
-	else
-	{
-		// TODO: 在此添加加载代码
-	}
-}
+//void CWinUIDoc::Serialize(CArchive& ar)
+//{
+//	if (ar.IsStoring())
+//	{
+//		// TODO: 在此添加存储代码
+//	}
+//	else
+//	{
+//		// TODO: 在此添加加载代码
+//	}
+//}
 
 // UNDONE: 删除
 #ifdef SHARED_HANDLERS
 
 // 缩略图的支持
-void CWinUIDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
-{
-	// 修改此代码以绘制文档数据
-	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
-
-	CString strText = _T("TODO: implement thumbnail drawing here");
-	LOGFONT lf;
-
-	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
-	pDefaultGUIFont->GetLogFont(&lf);
-	lf.lfHeight = 36;
-
-	CFont fontDraw;
-	fontDraw.CreateFontIndirect(&lf);
-
-	CFont* pOldFont = dc.SelectObject(&fontDraw);
-	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
-	dc.SelectObject(pOldFont);
-}
+//void CWinUIDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+//{
+//	// 修改此代码以绘制文档数据
+//	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+//
+//	CString strText = _T("TODO: implement thumbnail drawing here");
+//	LOGFONT lf;
+//
+//	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+//	pDefaultGUIFont->GetLogFont(&lf);
+//	lf.lfHeight = 36;
+//
+//	CFont fontDraw;
+//	fontDraw.CreateFontIndirect(&lf);
+//
+//	CFont* pOldFont = dc.SelectObject(&fontDraw);
+//	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+//	dc.SelectObject(pOldFont);
+//}
 
 // 搜索处理程序的支持
-void CWinUIDoc::InitializeSearchContent()
-{
-	CString strSearchContent;
-	// 从文档数据设置搜索内容。
-	// 内容部分应由“;”分隔
+//void CWinUIDoc::InitializeSearchContent()
+//{
+//	CString strSearchContent;
+//	// 从文档数据设置搜索内容。
+//	// 内容部分应由“;”分隔
+//
+//	// 例如:  strSearchContent = _T("point;rectangle;circle;ole object;")；
+//	SetSearchContent(strSearchContent);
+//}
 
-	// 例如:  strSearchContent = _T("point;rectangle;circle;ole object;")；
-	SetSearchContent(strSearchContent);
-}
-
-void CWinUIDoc::SetSearchContent(const CString& value)
-{
-	if (value.IsEmpty())
-	{
-		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
-	}
-	else
-	{
-		CMFCFilterChunkValueImpl *pChunk = NULL;
-		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
-		if (pChunk != NULL)
-		{
-			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
-			SetChunkValue(pChunk);
-		}
-	}
-}
+//void CWinUIDoc::SetSearchContent(const CString& value)
+//{
+//	if (value.IsEmpty())
+//	{
+//		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+//	}
+//	else
+//	{
+//		CMFCFilterChunkValueImpl *pChunk = NULL;
+//		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+//		if (pChunk != NULL)
+//		{
+//			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+//			SetChunkValue(pChunk);
+//		}
+//	}
+//}
 
 #endif // SHARED_HANDLERS
 
 // CWinUIDoc 诊断
 // UNDONE: 删除
 #ifdef _DEBUG
-void CWinUIDoc::AssertValid() const
-{
-	CDocument::AssertValid();
-}
+//void CWinUIDoc::AssertValid() const
+//{
+//	CDocument::AssertValid();
+//}
 
-void CWinUIDoc::Dump(CDumpContext& dc) const
-{
-	CDocument::Dump(dc);
-}
+//void CWinUIDoc::Dump(CDumpContext& dc) const
+//{
+//	CDocument::Dump(dc);
+//}
 #endif //_DEBUG
 
 
--- src/WinUI/WinUIDoc.h
+++ src/WinUI/WinUIDoc.h
@@ -29,18 +29,18 @@ public:
 // 重写
 public:
 	//virtual BOOL OnNewDocument();
-	virtual void Serialize(CArchive& ar);
+//	virtual void Serialize(CArchive& ar);
 #ifdef SHARED_HANDLERS
-	virtual void InitializeSearchContent();
-	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+//	virtual void InitializeSearchContent();
+//	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
 #endif // SHARED_HANDLERS
 
 // 实现
 public:
 	virtual ~CWinUIDoc();
 #ifdef _DEBUG
-	virtual void AssertValid() const;
-	virtual void Dump(CDumpContext& dc) const;
+//	virtual void AssertValid() const;
+//	virtual void Dump(CDumpContext& dc) const;
 #endif
 
 protected:
@@ -52,7 +52,7 @@ protected:
 
 #ifdef SHARED_HANDLERS
 	// 用于为搜索处理程序设置搜索内容的 Helper 函数
-	void SetSearchContent(const CString& value);
+//	void SetSearchContent(const CString& value);
 #endif // SHARED_HANDLERS
 public:
 	virtual BOOL OnOpenDocument(LPCTSTR lpszPathName);
