0c66afee0c57012493728efe1e03325e2ef0bbb1
shuhaoc <shuhaoc@qq.com>
2014-01-18 23:11:09 +0800

    删除CWinUIDoc中无用方法

--- src/WinUI/WinUIDoc.cpp
+++ src/WinUI/WinUIDoc.cpp
@@ -3,16 +3,7 @@
 //
 
 #include "stdafx.h"
-// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
-// ATL 项目中进行定义，并允许与该项目共享文档代码。
-#ifndef SHARED_HANDLERS
-#include "WinUI.h"
-#endif
-
 #include "WinUIDoc.h"
-
-#include <propkey.h>
-
 #include "ModelFactory.h"
 #include "ILogQuery.h"
 #include "LogQueryResult.h"
@@ -36,7 +27,6 @@ END_MESSAGE_MAP()
 CWinUIDoc::CWinUIDoc()
 	: queryResult(NULL)
 {
-	// TODO: 在此添加一次性构造代码
 	logQuery = ModelFactory::GetInstance()->CreateLogQuery();
 	queryResult = new LogQueryResult();
 }
@@ -52,112 +42,11 @@ void CWinUIDoc::setQueryResult(LogQueryResult* queryResult) {
 	this->queryResult = queryResult;
 }
 
-// UNDONE: 重写此方法，禁用新建
-//BOOL CWinUIDoc::OnNewDocument()
-//{
-//	if (!CDocument::OnNewDocument())
-//		return FALSE;
-//
-//	// TODO: 在此添加重新初始化代码
-//	// (SDI 文档将重用该文档)
-//
-//	return TRUE;
-//}
-
-// CWinUIDoc 序列化
-
-// UNDONE: 删除
-void CWinUIDoc::Serialize(CArchive& ar)
-{
-	if (ar.IsStoring())
-	{
-		// TODO: 在此添加存储代码
-	}
-	else
-	{
-		// TODO: 在此添加加载代码
-	}
-}
-
-// UNDONE: 删除
-#ifdef SHARED_HANDLERS
-
-// 缩略图的支持
-void CWinUIDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
-{
-	// 修改此代码以绘制文档数据
-	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
-
-	CString strText = _T("TODO: implement thumbnail drawing here");
-	LOGFONT lf;
-
-	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
-	pDefaultGUIFont->GetLogFont(&lf);
-	lf.lfHeight = 36;
-
-	CFont fontDraw;
-	fontDraw.CreateFontIndirect(&lf);
-
-	CFont* pOldFont = dc.SelectObject(&fontDraw);
-	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
-	dc.SelectObject(pOldFont);
-}
-
-// 搜索处理程序的支持
-void CWinUIDoc::InitializeSearchContent()
-{
-	CString strSearchContent;
-	// 从文档数据设置搜索内容。
-	// 内容部分应由“;”分隔
-
-	// 例如:  strSearchContent = _T("point;rectangle;circle;ole object;")；
-	SetSearchContent(strSearchContent);
-}
-
-void CWinUIDoc::SetSearchContent(const CString& value)
-{
-	if (value.IsEmpty())
-	{
-		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
-	}
-	else
-	{
-		CMFCFilterChunkValueImpl *pChunk = NULL;
-		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
-		if (pChunk != NULL)
-		{
-			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
-			SetChunkValue(pChunk);
-		}
-	}
-}
-
-#endif // SHARED_HANDLERS
-
-// CWinUIDoc 诊断
-// UNDONE: 删除
-#ifdef _DEBUG
-void CWinUIDoc::AssertValid() const
-{
-	CDocument::AssertValid();
-}
-
-void CWinUIDoc::Dump(CDumpContext& dc) const
-{
-	CDocument::Dump(dc);
-}
-#endif //_DEBUG
-
-
-// CWinUIDoc 命令
-
-
 BOOL CWinUIDoc::OnOpenDocument(LPCTSTR lpszPathName)
 {
 	if (!CDocument::OnOpenDocument(lpszPathName))
 		return FALSE;
 
-	// TODO:  在此添加您专用的创建代码
 	m_strPathName = lpszPathName;
 	clock_t begin = ::clock();
 	logQuery->load(m_strPathName.GetBuffer());
@@ -169,9 +58,9 @@ BOOL CWinUIDoc::OnOpenDocument(LPCTSTR lpszPathName)
 	return TRUE;
 }
 
-
 BOOL CWinUIDoc::OnSaveDocument(LPCTSTR lpszPathName)
 {
-	// TODO: 在此添加专用代码和/或调用基类
 	return TRUE;
 }
+
+// CWinUIDoc 命令
--- src/WinUI/WinUIDoc.h
+++ src/WinUI/WinUIDoc.h
@@ -19,29 +19,17 @@ public:
 	ILogQuery* logQuery;
 	LogQueryResult* queryResult;
 
-	//enum {
-	//	UpdateView_FileOPen = 1			// 不能从0开始，因为OnInitialUpdate会调用OnUpdate
-	//};
-
 // 操作
 public:
 
 // 重写
 public:
-	//virtual BOOL OnNewDocument();
-	virtual void Serialize(CArchive& ar);
-#ifdef SHARED_HANDLERS
-	virtual void InitializeSearchContent();
-	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
-#endif // SHARED_HANDLERS
+	virtual BOOL OnOpenDocument(LPCTSTR lpszPathName);
+	virtual BOOL OnSaveDocument(LPCTSTR lpszPathName);
 
 // 实现
 public:
 	virtual ~CWinUIDoc();
-#ifdef _DEBUG
-	virtual void AssertValid() const;
-	virtual void Dump(CDumpContext& dc) const;
-#endif
 
 protected:
 	void setQueryResult(LogQueryResult* queryResult);
@@ -49,12 +37,4 @@ protected:
 // 生成的消息映射函数
 protected:
 	DECLARE_MESSAGE_MAP()
-
-#ifdef SHARED_HANDLERS
-	// 用于为搜索处理程序设置搜索内容的 Helper 函数
-	void SetSearchContent(const CString& value);
-#endif // SHARED_HANDLERS
-public:
-	virtual BOOL OnOpenDocument(LPCTSTR lpszPathName);
-	virtual BOOL OnSaveDocument(LPCTSTR lpszPathName);
 };
