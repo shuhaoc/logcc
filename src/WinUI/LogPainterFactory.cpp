b26c2c4d1ab30712932fd6b6dcc76861adcbee73
shuhaoc <shuhaoc@qq.com>
2014-01-19 05:20:03 +0800

    修改主视图选中行描画样式

--- src/WinUI/LogPainterFactory.cpp
+++ src/WinUI/LogPainterFactory.cpp
@@ -6,15 +6,18 @@
 class LogSingleLinePainter : public ILogItemPainter {
 	virtual void Draw(HDC hdc, const RECT& rect, const LogItem& item) {
 		int oldBkMode = ::SetBkMode(hdc, TRANSPARENT);
+		int oldTextColor = ::GetTextColor(hdc);
 
 		if (item.selected) {
-			HBRUSH bkgdBrush = ::CreateSolidBrush(0x00FF9933);
+			HBRUSH bkgdBrush = ::CreateSolidBrush(0x00C36832);
 			HGDIOBJ oldBrush = ::SelectObject(hdc, bkgdBrush);
 
 			::FillRect(hdc, &rect, bkgdBrush);
 
 			::SelectObject(hdc, oldBrush);
 			::DeleteObject(bkgdBrush);
+
+			::SetTextColor(hdc, 0x00FFFFFF);
 		}
 
 		std::basic_ostringstream<TCHAR> oss;
@@ -26,6 +29,7 @@ class LogSingleLinePainter : public ILogItemPainter {
 			DT_NOCLIP | DT_SINGLELINE | DT_END_ELLIPSIS | DT_NOPREFIX | DT_EXPANDTABS);
 
 		::SetBkMode(hdc, oldBkMode);
+		::SetTextColor(hdc, oldTextColor);
 	}
 };
 
