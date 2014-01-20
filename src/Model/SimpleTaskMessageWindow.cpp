8d64ab0d09b6a0e006fad82d934601d36c618315
shuhaoc <shuhaoc@qq.com>
2014-01-21 03:12:46 +0800

    补充遗漏文件

---
+++ src/Model/SimpleTaskMessageWindow.cpp
@@ -0,0 +1,24 @@
+﻿#include "stdafx.h"
+#include "SimpleTaskMessageWindow.h"
+
+UINT SimpleTaskMessageWindow::InternalMessageId = ::RegisterWindowMessage(_T("{B4DACDED-4AC4-4636-9BCC-EEFB76E24538}"));
+
+SimpleTaskMessageWindow::SimpleTaskMessageWindow() {
+	internalWnd = ::CreateWindow(WndClass.lpszClassName, NULL, 0, 0, 0, 0, 0, HWND_MESSAGE, NULL,
+		WndClass.hInstance, 0);
+}
+
+SimpleTaskMessageWindow::~SimpleTaskMessageWindow() {
+	::DestroyWindow(internalWnd);
+}
+
+LRESULT SimpleTaskMessageWindow::WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
+	if (message == InternalMessageId) {
+		SimpleTask* task = reinterpret_cast<SimpleTask*>(wparam);
+		(*task)();
+		delete task;
+		return 0;
+	} else {
+		return ::DefWindowProc(hwnd, message, wparam, lparam);
+	}
+}
---
+++ src/Model/SimpleTaskMessageWindow.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+typedef std::function<void ()> SimpleTask;
+
+class SimpleTaskMessageWindow {
+public:
+	SimpleTaskMessageWindow();
+	
+	~SimpleTaskMessageWindow();
+
+	void post(SimpleTask* task) {
+		::PostMessage(internalWnd, InternalMessageId, reinterpret_cast<WPARAM>(task), 0);
+	}
+
+	static LRESULT WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
+
+	static WNDCLASS WndClass;
+
+private:
+	static UINT InternalMessageId;
+
+	HWND internalWnd;
+};
