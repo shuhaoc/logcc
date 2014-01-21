9e7dec8ed2ceed0a87777ff2cbdfeb6e2adb125f
shuhaoc <shuhaoc@qq.com>
2014-01-21 19:26:47 +0800

    删除多线程读文件代码，需要的话checkout multi_thread_get_line

--- src/Model/LogQueryImpl.cpp
+++ src/Model/LogQueryImpl.cpp
@@ -5,8 +5,6 @@
 #include "LogQueryResult.h"
 #include "SimpleTaskMessageWindow.h"
 
-#define MULTI_THREAD_GET_LINE
-
 
 LogQueryImpl::LogQueryImpl()
 	: curQueryResult(new LogQueryResult())
@@ -40,69 +38,6 @@ bool LogQueryImpl::load(const tstring& filePath) {
 	size_t length = static_cast<size_t>(file.tellg());
 	file.seekg(0, ios_base::beg);
 
-#ifdef MULTI_THREAD_GET_LINE
-
-	size_t length1 = length / 2;
-	size_t length2 = length - length1;
-
-	char* buffer1 = new char[length1 + 1];
-	char* buffer2 = new char[length2];
-
-	streamsize read = 0;
-	while (file.good() && length1 - read) {
-		file.read(buffer1 + read, length1 - read);
-		read += file.gcount();
-	}
-	buffer1[read] = 0;
-	assert(read == length1);
-
-	read = 0;
-	while (file.good() && length2 - read) {
-		file.read(buffer2 + read, length2 - read);
-		read += file.gcount();
-	}
-	buffer2[read] = 0;
-	assert(read == length2);
-
-	vector<string> lines1, lines2;
-
-	boost::thread_group workers;
-
-	auto f = [] (char* buffer, vector<string>* logItems) {
-		istringstream iss(buffer);
-
-		string line;
-		while (iss.good()) {
-			std::getline(iss, line);
-			logItems->push_back(line);
-		}
-	};
-
-	workers.create_thread([f, buffer1, &lines1] () { f(buffer1, &lines1); });
-	workers.create_thread([f, buffer2, &lines2] () { f(buffer2, &lines2); });
-	workers.join_all();
-
-	if (lines1.size() && lines1.rbegin()->size() && *lines1.rbegin()->rbegin() != '\n') {
-		lines1.rbegin()->append(*lines2.begin());
-		lines1.insert(lines1.end(), lines2.begin() + 1, lines2.end());
-	} else {
-		lines1.insert(lines1.end(), lines2.begin(), lines2.end());
-	}
-
-	for (unsigned i = 0; i < lines1.size(); i++) {
-		string& line = lines1[i];
-
-		LogItem* item = new LogItem();
-		item->line = i + 1;
-		#ifdef _UNICODE
-		item->text = mrl::utility::codeconv::asciiToUnicode(line);
-		#else
-		item->text = line;
-		#endif
-		item->selected = false;
-		logItems.push_back(item);
-	}
-#else
 	char* buffer = new char[length];
 	streamsize read = 0;
 	while (file.good() && length - read) {
@@ -129,7 +64,6 @@ bool LogQueryImpl::load(const tstring& filePath) {
 		item->selected = false;
 		logItems.push_back(item);
 	}
-#endif
 	this->filePath = filePath;
 	startMonitor();
 	return true;
@@ -188,60 +122,58 @@ void LogQueryImpl::scrollTo(int y) {
 void LogQueryImpl::startMonitor() {
 	monitorThread = new boost::thread(([this] () {
 		monitoring = true;
-		HANDLE file = ::CreateFile(filePath.c_str(), GENERIC_READ, FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
-			NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 		while (monitoring) {
 			::Sleep(500);
-			if (1) {
-				if (1) {
-					// UNDONE: 文件读取代码复用
-					DWORD fileSize = ::GetFileSize(file, NULL);
-					char* buffer = new char[fileSize + 1];
-					DWORD readSize = 0;
-					// 从头重新读取文件
-					::SetFilePointer(file, 0, NULL, FILE_BEGIN);
-					if (::ReadFile(file, buffer, fileSize + 1, &readSize, NULL)) {
-						buffer[readSize] = 0;
-						istringstream iss(buffer);
+			// UNDONE: 文件读取代码复用
+			vector<LogItem*> logItems;
+			HANDLE file = ::CreateFile(filePath.c_str(), GENERIC_READ, FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
+				NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+			if (file != INVALID_HANDLE_VALUE) {
+				DWORD fileSize = ::GetFileSize(file, NULL);
+				char* buffer = new char[fileSize + 1];
+				DWORD readSize = 0;
+				// 从头重新读取文件
+				::SetFilePointer(file, 0, NULL, FILE_BEGIN);
+				if (::ReadFile(file, buffer, fileSize + 1, &readSize, NULL)) {
+					buffer[readSize] = 0;
+					istringstream iss(buffer);
 
-						vector<LogItem*> logItems;
-						string line;
-						unsigned lineNum = 0;
-						while (iss.good()) {
-							std::getline(iss, line);
-							LogItem* item = new LogItem();
-							item->line = ++lineNum;
-#ifdef _UNICODE
-							item->text = mrl::utility::codeconv::asciiToUnicode(line);
-#else
-							item->text = line;
-#endif
-							item->selected = false;
-							logItems.push_back(item);
-						}
-						// UNDONE: 简单比较
-						if (this->logItems.size() != logItems.size()) {
-							DEBUG_INFO(_T("监测到文件变化"));
-							LogQueryImpl* that = this;
-							taskWnd->post(new SimpleTask([that, logItems] () {
-								that->reset(logItems);
-							}));
-						} else {
-							for_each(logItems.begin(), logItems.end(), [] (LogItem* p) { delete p; });
-						}
-					} else {
-						// 读文件失败
-						// UNDONE: 以下代码重复
-						monitoring = false;
-						LogQueryImpl* that = this;
-						taskWnd->post(new SimpleTask([that] () {
-							that->clear();
-						}));
+					string line;
+					unsigned lineNum = 0;
+					while (iss.good()) {
+						std::getline(iss, line);
+						LogItem* item = new LogItem();
+						item->line = ++lineNum;
+	#ifdef _UNICODE
+						item->text = mrl::utility::codeconv::asciiToUnicode(line);
+	#else
+						item->text = line;
+	#endif
+						item->selected = false;
+						logItems.push_back(item);
 					}
+					::CloseHandle(file);
 				}
+				// UNDONE: 简单比较
+				if (this->logItems.size() != logItems.size()) {
+					DEBUG_INFO(_T("监测到文件变化"));
+					LogQueryImpl* that = this;
+					taskWnd->post(new SimpleTask([that, logItems] () {
+						that->reset(logItems);
+					}));
+				} else {
+					for_each(logItems.begin(), logItems.end(), [] (LogItem* p) { delete p; });
+				}
+			} else {
+				// 读文件失败
+				// UNDONE: 以下代码重复
+				monitoring = false;
+				LogQueryImpl* that = this;
+				taskWnd->post(new SimpleTask([that] () {
+					that->clear();
+				}));
 			}
 		} // while monitoring
-		::CloseHandle(file);
 		DEBUG_INFO(_T("监控线程退出！"));
 	}));
 }
