e144b6910b5408d8d0cce1dc0d2fe261e7c6ac1a
shuhaoc <shuhaoc@qq.com>
2014-01-29 23:58:15 +0800

    bugfix: 优化消息路由，只转发Controller要处理的消息，并在窗口销毁时删除路由，解决拖动滚动条后两次鼠标点击必然失效的bug

--- include/ControllerBase.h
+++ include/ControllerBase.h
@@ -4,15 +4,33 @@
 #include "IViewDataAware.h"
 #include "ViewBase.h"
 
-template <typename ModelT, typename ViewDataT>
-class ControllerBase : public CWnd, public IModelAware<ModelT>, public IViewDataAware<ViewDataT> {
+template <typename ModelT>
+class InternalControllerBase : public CWnd, public IModelAware<ModelT> {
 public:
-	ControllerBase(CWnd* parent) {
+	InternalControllerBase(CWnd* parent) {
 		Create(::AfxRegisterWndClass(0), NULL, 0, CRect(), parent, 0);
 	}
 
 	virtual void PostNcDestroy() { delete this; }
-	
+
+	void accept(UINT msg, WPARAM wparam, LPARAM lparam) {
+		const AFX_MSGMAP_ENTRY* entry = GetMessageMap()->lpEntries;
+		while (entry->nMessage != 0) {
+			if (entry->nMessage == msg) {
+				this->WindowProc(msg, wparam, lparam);
+			}
+			entry++;
+		}
+	}
+};
+
+
+template <typename ModelT, typename ViewDataT>
+class ControllerBase : public InternalControllerBase<ModelT>, public IViewDataAware<ViewDataT> {
+public:
+	ControllerBase(CWnd* parent) : InternalControllerBase(parent) {
+	}
+
 	void updateViewData() {
 		static_cast<ViewBase<ModelT, ViewDataT>*>(getViewData())->onSubmit();
 	}
--- include/ControllerRoute.h
+++ include/ControllerRoute.h
@@ -11,25 +11,24 @@ public:
 		ViewBase<ModelT, ViewDataT>* view, ControllerBase<ModelT, ViewDataT>* controller, IModelFactory<ModelT>* factory);
 	
 private:
-	static std::hash_map<HWND, HWND> routeMap;
+	static std::hash_map<HWND, InternalControllerBase<ModelT>*> routeMap;
 
 	static LRESULT CALLBACK wndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 	static std::hash_map<HWND, WNDPROC> originWndProcMap;
 };
 
-template <typename ModelT> std::hash_map<HWND, HWND> ControllerRoute<ModelT>::routeMap;
+template <typename ModelT> std::hash_map<HWND, InternalControllerBase<ModelT>*> ControllerRoute<ModelT>::routeMap;
 
 template <typename ModelT> std::hash_map<HWND, WNDPROC> ControllerRoute<ModelT>::originWndProcMap;
 
 template <typename ModelT> template<typename ViewDataT> void ControllerRoute<ModelT>::addRoute(
 		ViewBase<ModelT, ViewDataT>* view, ControllerBase<ModelT, ViewDataT>* controller, IModelFactory<ModelT>* factory) {
 	CWnd* viewWnd = dynamic_cast<CWnd*>(view);
-	CWnd* controllerWnd = dynamic_cast<CWnd*>(controller);
 	
 	assert(viewWnd && controller);
 	assert(!routeMap[viewWnd->GetSafeHwnd()]);
-	routeMap[viewWnd->GetSafeHwnd()] = controllerWnd->GetSafeHwnd();
+	routeMap[viewWnd->GetSafeHwnd()] = controller;
 	
 	originWndProcMap[viewWnd->GetSafeHwnd()] = reinterpret_cast<WNDPROC>(::SetWindowLong(
 		viewWnd->GetSafeHwnd(), GWL_WNDPROC, reinterpret_cast<LONG>(ControllerRoute::wndProc)));
@@ -42,11 +41,16 @@ template <typename ModelT> template<typename ViewDataT> void ControllerRoute<Mod
 }
 
 template <typename ModelT> LRESULT CALLBACK ControllerRoute<ModelT>::wndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-	// UNDONE: 优化，针对WM_DESTROY消息移除哈希表项
-	HWND controller = routeMap[hwnd];
 	WNDPROC originProc = originWndProcMap[hwnd];
 	LRESULT result = ::CallWindowProc(originProc, hwnd, msg, wparam, lparam);
-	assert(controller);
-	::SendMessage(controller, msg, wparam, lparam);
+	if (msg == WM_DESTROY) {
+		routeMap.erase(hwnd);		
+		::SetWindowLong(hwnd, GWL_WNDPROC, reinterpret_cast<LONG>(originProc));
+		originWndProcMap.erase(hwnd);
+	} else {
+		InternalControllerBase<ModelT>* controller = routeMap[hwnd];
+		assert(controller);
+		controller->accept(msg, wparam, lparam);
+	}
 	return result;
 }
