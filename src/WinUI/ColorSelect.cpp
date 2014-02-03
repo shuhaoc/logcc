575151984986d16f70c2ac8f9b9ffd9eeb209611
shuhaoc <shuhaoc@qq.com>
2014-02-03 20:44:22 +0800

    添加CColorSelect类

---
+++ src/WinUI/ColorSelect.cpp
@@ -0,0 +1,27 @@
+﻿#include "StdAfx.h"
+#include "ColorSelect.h"
+
+CColorSelect::CColorSelect(void) {
+}
+
+CColorSelect::~CColorSelect(void) {
+}
+BEGIN_MESSAGE_MAP(CColorSelect, CWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+void CColorSelect::OnPaint() {
+	CPaintDC dc(this); // device context for painting
+	// TODO: 在此处添加消息处理程序代码
+	// 不为绘图消息调用 CWnd::OnPaint()
+
+	//绘制背景
+	CRect rcOld;
+	GetClientRect(rcOld);
+	CPen pen(PS_SOLID,1,m_Color);
+	CBrush bkgBrush(m_Color);
+	CPen *oldpen = dc.SelectObject(&pen);
+	dc.Rectangle(&rcOld);
+	dc.FillRect(rcOld,&bkgBrush);
+	dc.SelectObject(oldpen);
+}
---
+++ src/WinUI/ColorSelect.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "afxwin.h"
+
+class CColorSelect :
+	public CButton {
+public:
+	CColorSelect(void);
+	~CColorSelect(void);
+
+private:
+	COLORREF m_Color;		//颜色
+public:
+	DECLARE_MESSAGE_MAP()
+	afx_msg void OnPaint();
+public:
+
+	void SetColor(COLORREF color) {
+		m_Color = color;
+	}
+};
