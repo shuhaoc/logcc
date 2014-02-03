e6942be447a0c5ba85cb2556325bcfa836c83e1a
shuhaoc <shuhaoc@qq.com>
2014-02-03 21:46:13 +0800

    添加Pattern类

---
+++ src/Model/Pattern.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "Pattern.h"
+
+const string Pattern::tableName = "Pattern";
+
+const string Pattern::keyName = "id";
+
+const array<string, 3> Pattern::fieldNames = { "id", "textColor", "backColor" };
