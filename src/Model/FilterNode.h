ae42013de69b1744bfdca9755deefb23fa57f28d
shuhaoc <shuhaoc@qq.com>
2014-02-08 10:51:21 +0800

    集成自实现过滤表达式解析器

---
+++ src/Model/FilterNode.h
@@ -0,0 +1,41 @@
+#pragma once
+
+
+struct FilterNode {
+	int id;
+
+	FilterNode();
+
+	virtual ~FilterNode() { }
+
+	virtual void print() const;
+
+	virtual bool match(const std::string& data) const = 0;
+};
+
+struct OpNode : public FilterNode {
+	std::string op;
+	FilterNode* left;
+	FilterNode* right;
+
+	OpNode() : left(nullptr), right(nullptr) {
+	}
+
+	virtual ~OpNode() {
+		delete left;
+		delete right;
+	}
+
+	virtual void print() const;
+	
+	virtual bool match(const std::string& data) const;
+};
+
+struct TextNode : public FilterNode {
+	std::string word;
+
+	virtual void print() const;
+	
+	virtual bool match(const std::string& data) const;
+};
+
